2250e70b5cdac70987b2b49c652a175a33a3406f
Lucas <kauanferreira99@hotmail.com>
2021-02-07 14:18:45 -0300

    feat:modif(3)

--- js/scripts.js
+++ js/scripts.js
@@ -89,7 +89,7 @@ const DOM = {
         <td class="${CSSclass}">${amount}</td>
         <td class="date">${transaction.date}</td>
         <td>
-            <img onclick="Transaction.remove(${index})" id="img-remove" src="../assets/minus.svg" alt="Remover transação">
+            <img onclick="Transaction.remove(${index})" id="remove" src="../assets/minus.svg" alt="Remover transação">
         </td>
         `
 
